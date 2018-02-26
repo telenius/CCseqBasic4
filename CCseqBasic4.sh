0b14435793a2f396e609df0918ac0240e65a34e2
Jelena Telenius <jelena.telenius@gmail.com>
2018-02-26 13:46:38 +0000

    Disabling the makeSymbolicLinks subroutine : no symlinks in public from now on

--- CCseqBasic4.sh
+++ CCseqBasic4.sh
@@ -1320,7 +1320,7 @@ generateCombinedDataHub
 # Cleaning up after ourselves ..
 
 cleanUpRunFolder
-makeSymbolicLinks
+# makeSymbolicLinks
 
 # Data hub address (print to stdout) ..
 updateHub_part3final
