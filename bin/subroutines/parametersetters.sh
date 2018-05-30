de8958aff84e4a34735991e58c8c314ec4dce50f
telenius <jelena.telenius@gmail.com>
2018-05-30 12:07:21 +0100

    fix to yesterdays fix

--- bin/subroutines/parametersetters.sh
+++ bin/subroutines/parametersetters.sh
@@ -37,10 +37,10 @@ setParameters(){
 # Listing current limitations, exiting if needed :
 
 if [ "${LOWERCASE_M}" -ne 0 ] && [ "${CAPITAL_M}" -ne 0 ];
-then
+then   
     printThis="Both -m and -M parameters cannot be set at the same time\nEXITING"
     printToLogFile
-  exit 1
+  exit 1  
 fi
 
 #----------------------------------------------
